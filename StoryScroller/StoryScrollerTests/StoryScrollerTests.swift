9472f44594db57c87a0d9585bf0e418463d64c05
Paul Ehrhardt <ehrpaulhardt@gmail.com>
2025-04-12 11:26:28 +0100

    Basic project setup

---
+++ StoryScroller/StoryScrollerTests/StoryScrollerTests.swift
@@ -0,0 +1,17 @@
+//
+//  StoryScrollerTests.swift
+//  StoryScrollerTests
+//
+//  Created by Paul Ehrhardt on 12/4/25.
+//
+
+import Testing
+@testable import StoryScroller
+
+struct StoryScrollerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
